5ee8a49760f5e9605533453ae0d95e019ff9094c
nitrotap <kartikinpublic@gmail.com>
2021-12-27 23:07:56 -0700

    adding enemies

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,11 +6,24 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+// var enemyName = "Roborto";
 var enemyHealth = 50;
 var enemyAttack = 12;
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+console.log(enemyNames);
+console.log(enemyNames.length);
 
-var fight = function () {
+for (var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+}
+
+
+
+
+
+var fight = function (enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -60,10 +73,9 @@ var fight = function () {
 
     } else {
         window.alert("You need to choose a valid option. Try again!");
-        
     }
-
 }
 
-fight();
-
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+  }
